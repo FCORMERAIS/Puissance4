eec85768d2403967a88542b03098334f000d6dc3
Mostapha <mostapha.tourabi@ynov.com>
2022-10-10 15:33:55 +0200

    Menu ameliorer

--- Power4/src/menu.java
+++ Power4/src/menu.java
@@ -19,10 +19,26 @@ public class menu {
             case 2:
                 System.out.println("You chose to see the rules");
                 System.out.println("The object of the game is to line up 4 pawns on a grid with 6 rows and 7 columns. Each player plays alternately by dropping one of their tokens from the top of one of the columns. This token therefore fills the lowest unoccupied space in the column. Good luck !");
-                break;
+                Scanner sc2 = new Scanner(System.in);
+                String choice2 = sc2.nextLine();
+                if (choice2.equals("1")) {
+                    System.out.println("You chose to play");
+                } else if (choice2.equals("2")) {
+                    System.out.println("You chose to see the rules");
+                } else if (choice2.equals("3")) {
+                    System.out.println("You chose to exit");
+                }
             case 3:
                 System.out.println("You chose to exit");
-                break;
+                Scanner sc3 = new Scanner(System.in);
+                String choice3 = sc3.nextLine();
+                if (choice3.equals("1")) {
+                    System.out.println("You chose to play");
+                } else if (choice3.equals("2")) {
+                    System.out.println("You chose to see the rules");
+                } else if (choice3.equals("3")) {
+                    System.out.println("You chose to exit");
+                }
             default:
                 System.out.println("You chose an invalid option");
                 break;
