2d62457b59b8a9a5b0ff1da6b343d955b7550cc9
Mostapha <mostapha.tourabi@ynov.com>
2022-10-14 16:17:24 +0200

    Menu finalisée

--- Power4/src/App.java
+++ Power4/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         menu.Menu();
-        Display.printGrid();
+        // Display.printGrid();
     }
 }
--- Power4/src/menu.java
+++ Power4/src/menu.java
@@ -9,42 +9,64 @@ public class menu {
         System.out.println("1. Play");
         System.out.println("2. Rules");
         System.out.println("3. Exit");
-    try {
+    // try {
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         switch (choice) {
             case 1:
                 System.out.println("You chose to play");
+                Display.printGrid();
                 break;
             case 2:
                 System.out.println("You chose to see the rules");
                 System.out.println("The object of the game is to line up 4 pawns on a grid with 6 rows and 7 columns. Each player plays alternately by dropping one of their tokens from the top of one of the columns. This token therefore fills the lowest unoccupied space in the column. Good luck !");
+                System.out.println("1. Play");
+                System.out.println("2. Rules");
+                System.out.println("3. Exit");
                 Scanner sc2 = new Scanner(System.in);
                 String choice2 = sc2.nextLine();
                 if (choice2.equals("1")) {
                     System.out.println("You chose to play");
+                    Display.printGrid();
+                    break;
                 } else if (choice2.equals("2")) {
                     System.out.println("You chose to see the rules");
                 } else if (choice2.equals("3")) {
                     System.out.println("You chose to exit");
+                    break;
                 }
             case 3:
                 System.out.println("You chose to exit");
-                Scanner sc3 = new Scanner(System.in);
-                String choice3 = sc3.nextLine();
-                if (choice3.equals("1")) {
-                    System.out.println("You chose to play");
-                } else if (choice3.equals("2")) {
-                    System.out.println("You chose to see the rules");
-                } else if (choice3.equals("3")) {
-                    System.out.println("You chose to exit");
-                }
+                break;
+                // Scanner sc3 = new Scanner(System.in);
+                // String choice3 = sc3.nextLine();
+                // if (choice3.equals("1")) {
+                //     System.out.println("You chose to play");
+                // } else if (choice3.equals("2")) {
+                //     System.out.println("You chose to see the rules");
+                // } else if (choice3.equals("3")) {
+                //     System.out.println("You chose to exit");
+                // }
             default:
                 System.out.println("You chose an invalid option");
                 break;
         }
-    } catch (Exception e) {
-        System.out.println("You chose an invalid option");
-    }
+    //    exemple // gameover = false
+        // gameover = true
+        // if (gameover == true) {
+        //     System.out.println("Do you want to play again? (Y/N)");
+        //     Scanner sc4 = new Scanner(System.in);
+        //     String choice4 = sc4.nextLine();
+        //     if (choice4.equals("Y")) {
+        //         System.out.println("You chose to play again");
+        //         Display.printGrid();
+        //     } else if (choice4.equals("N")) {
+        //         System.out.println("You chose to exit");
+        //     }
+        // }
+    // } catch (Exception e) {
+    //     System.out.println("You chose an invalid option");
+    // }
     }  
 }
+
